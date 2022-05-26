6beddda703eccdc4b083148649fdbb18ad71213e
Binamra7 <binamra7khadka6@gmail.com>
2022-05-26 18:34:22 +0530

    updated heroku api route

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -5,109 +5,109 @@ import ActiveBugs from "./ActiveBugs";
 import ResolvedBugs from "./ResolvedBugs";
 
 function Dashboard() {
-  const [bugs, setBugs] = useState({
-    active: [],
-    resolved: [],
-  });
+	const [bugs, setBugs] = useState({
+		active: [],
+		resolved: [],
+	});
 
-  useEffect(() => {
-    axios.get("http://localhost:5000/bug/").then((res) => {
-      //add those bugs which have bugResolved value of false to active and
-      // others to resolved
-      setBugs({
-        active: res.data.filter((bug) => bug.bugResolved == false),
-        resolved: res.data.filter((bug) => bug.bugResolved === true),
-      });
-    });
-  }, []);
+	useEffect(() => {
+		axios.get("https://bug-reporter-api.herokuapp.com/bug/").then((res) => {
+			//add those bugs which have bugResolved value of false to active and
+			// others to resolved
+			setBugs({
+				active: res.data.filter((bug) => bug.bugResolved == false),
+				resolved: res.data.filter((bug) => bug.bugResolved === true),
+			});
+		});
+	}, []);
 
-  const [status, setStatus] = useState("active");
+	const [status, setStatus] = useState("active");
 
-  const onResolvedRequest = (bug) => {
-    axios
-      .put(`http://localhost:5000/bug/update/${bug._id}`, {
-        bugTitle: bug.bugTitle,
-        bugDescription: bug.bugDescription,
-        bugSeverity: bug.bugSeverity,
-        bugResolved: true,
-      })
-      .then((res) => {
-        setBugs({
-          active: bugs.active.filter((b) => b._id !== bug._id),
-          resolved: [...bugs.resolved, bug],
-        });
-      });
-  };
+	const onResolvedRequest = (bug) => {
+		axios
+			.put(`http://localhost:5000/bug/update/${bug._id}`, {
+				bugTitle: bug.bugTitle,
+				bugDescription: bug.bugDescription,
+				bugSeverity: bug.bugSeverity,
+				bugResolved: true,
+			})
+			.then((res) => {
+				setBugs({
+					active: bugs.active.filter((b) => b._id !== bug._id),
+					resolved: [...bugs.resolved, bug],
+				});
+			});
+	};
 
-  const onDeleteBug = (bug) => {
-    axios.delete(`http://localhost:5000/bug/${bug._id}`).then((res) => {
-      setBugs({
-        active: bugs.active.filter((b) => b._id !== bug._id),
-        resolved: bugs.resolved.filter((b) => b._id !== bug._id),
-      });
-    });
-  };
+	const onDeleteBug = (bug) => {
+		axios.delete(`http://localhost:5000/bug/${bug._id}`).then((res) => {
+			setBugs({
+				active: bugs.active.filter((b) => b._id !== bug._id),
+				resolved: bugs.resolved.filter((b) => b._id !== bug._id),
+			});
+		});
+	};
 
-  return (
-    <div className="dashboard">
-      <h1 style={{ margin: "2rem 0" }}>Welcome to dashboard</h1>
-      <div className="dashboard__navbar">
-        <button
-          style={
-            status === "active"
-              ? {
-                  backgroundColor: "#ffbfbf",
-                  color: "black",
-                  border: "none",
-                }
-              : { border: "none" }
-          }
-          onClick={() => setStatus("active")}
-          className="active"
-        >
-          Active Bugs ({bugs.active.length})
-        </button>
-        <button
-          style={
-            status === "resolved"
-              ? {
-                  backgroundColor: "rgb(157 225 143)",
-                  color: "black",
-                  border: "none",
-                }
-              : { border: "none" }
-          }
-          onClick={() => setStatus("resolved")}
-          className="resolved"
-        >
-          Resolved Bugs ({bugs.resolved.length})
-        </button>
-      </div>
-      <div
-        className="bug__container"
-        style={
-          status === "resolved"
-            ? {
-                backgroundColor: "rgb(157 225 143)",
-              }
-            : { border: "none" }
-        }
-      >
-        {status === "active" ? (
-          <ActiveBugs
-            bugs={bugs.active}
-            onUpdateBugs={onResolvedRequest}
-            onDeleteFromActive={onDeleteBug}
-          />
-        ) : (
-          <ResolvedBugs
-            bugs={bugs.resolved}
-            onDeleteFromResolved={onDeleteBug}
-          />
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className="dashboard">
+			<h1 style={{ margin: "2rem 0" }}>Welcome to dashboard</h1>
+			<div className="dashboard__navbar">
+				<button
+					style={
+						status === "active"
+							? {
+									backgroundColor: "#ffbfbf",
+									color: "black",
+									border: "none",
+							  }
+							: { border: "none" }
+					}
+					onClick={() => setStatus("active")}
+					className="active"
+				>
+					Active Bugs ({bugs.active.length})
+				</button>
+				<button
+					style={
+						status === "resolved"
+							? {
+									backgroundColor: "rgb(157 225 143)",
+									color: "black",
+									border: "none",
+							  }
+							: { border: "none" }
+					}
+					onClick={() => setStatus("resolved")}
+					className="resolved"
+				>
+					Resolved Bugs ({bugs.resolved.length})
+				</button>
+			</div>
+			<div
+				className="bug__container"
+				style={
+					status === "resolved"
+						? {
+								backgroundColor: "rgb(157 225 143)",
+						  }
+						: { border: "none" }
+				}
+			>
+				{status === "active" ? (
+					<ActiveBugs
+						bugs={bugs.active}
+						onUpdateBugs={onResolvedRequest}
+						onDeleteFromActive={onDeleteBug}
+					/>
+				) : (
+					<ResolvedBugs
+						bugs={bugs.resolved}
+						onDeleteFromResolved={onDeleteBug}
+					/>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Dashboard;
--- client/src/components/ReportBug.jsx
+++ client/src/components/ReportBug.jsx
@@ -4,155 +4,155 @@ import Modal from "./modal/Modal";
 import "./reportBug.css";
 
 function ReportBug() {
-  const [bug, setBug] = useState({
-    bugSeverity: "",
-    bugTitle: "",
-    bugDescription: "",
-  });
+	const [bug, setBug] = useState({
+		bugSeverity: "",
+		bugTitle: "",
+		bugDescription: "",
+	});
 
-  const [modal, setModal] = useState(false);
-  const [isValid, setIsValid] = useState(true);
-  const [severity, setSeverity] = useState("");
-  const [bugTitle, setBugTitle] = useState("");
-  const [bugDescription, setBugDescription] = useState("");
-  const [check, setCheck] = useState(false);
+	const [modal, setModal] = useState(false);
+	const [isValid, setIsValid] = useState(true);
+	const [severity, setSeverity] = useState("");
+	const [bugTitle, setBugTitle] = useState("");
+	const [bugDescription, setBugDescription] = useState("");
+	const [check, setCheck] = useState(false);
 
-  const handleTitleChange = (e) => {
-    setBugTitle(e.target.value);
-    setBug((prevBug) => ({
-      bugTitle: e.target.value,
-      bugDescription: prevBug.bugDescription,
-      bugSeverity: prevBug.bugSeverity,
-    }));
-  };
+	const handleTitleChange = (e) => {
+		setBugTitle(e.target.value);
+		setBug((prevBug) => ({
+			bugTitle: e.target.value,
+			bugDescription: prevBug.bugDescription,
+			bugSeverity: prevBug.bugSeverity,
+		}));
+	};
 
-  const handleDescriptionChange = (e) => {
-    setBug((prevBug) => ({
-      bugTitle: prevBug.bugTitle,
-      bugDescription: e.target.value,
-      bugSeverity: prevBug.bugSeverity,
-    }));
+	const handleDescriptionChange = (e) => {
+		setBug((prevBug) => ({
+			bugTitle: prevBug.bugTitle,
+			bugDescription: e.target.value,
+			bugSeverity: prevBug.bugSeverity,
+		}));
 
-    setBugDescription(e.target.value);
-  };
+		setBugDescription(e.target.value);
+	};
 
-  const handleRadioChange = (e) => {
-    setCheck(true);
-    setBug((prevBug) => ({
-      bugTitle: prevBug.bugTitle,
-      bugDescription: prevBug.bugDescription,
-      bugSeverity: e.target.value,
-    }));
-    setSeverity(e.target.value);
-  };
+	const handleRadioChange = (e) => {
+		setCheck(true);
+		setBug((prevBug) => ({
+			bugTitle: prevBug.bugTitle,
+			bugDescription: prevBug.bugDescription,
+			bugSeverity: e.target.value,
+		}));
+		setSeverity(e.target.value);
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (
-      !(
-        bugTitle.trim().length > 0 &&
-        bugDescription.trim().length > 0 &&
-        severity.trim().length > 0
-      )
-    ) {
-      setIsValid(false);
-      alert("Please fill all the fields properly");
-    } else {
-      setIsValid(true);
-      setBugDescription("");
-      setBugTitle("");
-      setModal(true);
-      axios.post("http://localhost:5000/bug/add", bug);
-    }
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (
+			!(
+				bugTitle.trim().length > 0 &&
+				bugDescription.trim().length > 0 &&
+				severity.trim().length > 0
+			)
+		) {
+			setIsValid(false);
+			alert("Please fill all the fields properly");
+		} else {
+			setIsValid(true);
+			setBugDescription("");
+			setBugTitle("");
+			setModal(true);
+			axios.post("https://bug-reporter-api.herokuapp.com/bug/add", bug);
+		}
+	};
 
-  return (
-    <div className="report_bug">
-      {modal && (
-        <Modal
-          heading="Bug Submitted Successfully!"
-          show={setModal}
-          submit={true}
-        />
-      )}
-      <form onSubmit={handleSubmit}>
-        <h1 style={{ justifyContent: "left", marginBottom: "2rem" }}>
-          Report a bug
-        </h1>
+	return (
+		<div className="report_bug">
+			{modal && (
+				<Modal
+					heading="Bug Submitted Successfully!"
+					show={setModal}
+					submit={true}
+				/>
+			)}
+			<form onSubmit={handleSubmit}>
+				<h1 style={{ justifyContent: "left", marginBottom: "2rem" }}>
+					Report a bug
+				</h1>
 
-        {/*bug title and its description*/}
-        <label className="bug__explanation">
-          Bug Title:
-          <br />
-          <input
-          autoComplete="off"
-            style={!isValid ? { borderColor: "red" } : { borderColor: "black" }}
-            value={bugTitle}
-            type="text"
-            name="bugName"
-            className="bug__title"
-            onChange={handleTitleChange}
-          />
-        </label>
-        <label className="bug__explanation">
-          Bug Description:
-          <br />
-          <textarea
-            autoComplete="off"
-            style={!isValid ? { borderColor: "red" } : { borderColor: "black" }}
-            value={bugDescription}
-            type="text"
-            name="bugDescription"
-            className="bug__desc"
-            onChange={handleDescriptionChange}
-          />
-        </label>
-        {/*bug severity*/}
-        <div className="radio__container">
-          <h2>How severe is it?</h2>
-          <label style={{ color: "#e50000" }}>
-            <input
-              onChange={handleRadioChange}
-              className="urgency__input"
-              type="radio"
-              name="severity"
-              value="high"
-            />
-            High
-          </label>
-          <br />
-          <label style={{ color: "#b55809" }}>
-            <input
-              onChange={handleRadioChange}
-              className="urgency__input"
-              type="radio"
-              name="severity"
-              value="medium"
-            />
-            Medium
-          </label>
-          <br />
-          <label style={{ color: "#848d03" }}>
-            <input
-              onChange={handleRadioChange}
-              className="urgency__input"
-              type="radio"
-              name="severity"
-              value="low"
-            />
-            Low
-          </label>
-          <br />
-        </div>
-        <button
-          style={
-            !isValid ? { backgroundColor: "red" } : { backgroundColor: "black" }
-          }
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+				{/*bug title and its description*/}
+				<label className="bug__explanation">
+					Bug Title:
+					<br />
+					<input
+						autoComplete="off"
+						style={!isValid ? { borderColor: "red" } : { borderColor: "black" }}
+						value={bugTitle}
+						type="text"
+						name="bugName"
+						className="bug__title"
+						onChange={handleTitleChange}
+					/>
+				</label>
+				<label className="bug__explanation">
+					Bug Description:
+					<br />
+					<textarea
+						autoComplete="off"
+						style={!isValid ? { borderColor: "red" } : { borderColor: "black" }}
+						value={bugDescription}
+						type="text"
+						name="bugDescription"
+						className="bug__desc"
+						onChange={handleDescriptionChange}
+					/>
+				</label>
+				{/*bug severity*/}
+				<div className="radio__container">
+					<h2>How severe is it?</h2>
+					<label style={{ color: "#e50000" }}>
+						<input
+							onChange={handleRadioChange}
+							className="urgency__input"
+							type="radio"
+							name="severity"
+							value="high"
+						/>
+						High
+					</label>
+					<br />
+					<label style={{ color: "#b55809" }}>
+						<input
+							onChange={handleRadioChange}
+							className="urgency__input"
+							type="radio"
+							name="severity"
+							value="medium"
+						/>
+						Medium
+					</label>
+					<br />
+					<label style={{ color: "#848d03" }}>
+						<input
+							onChange={handleRadioChange}
+							className="urgency__input"
+							type="radio"
+							name="severity"
+							value="low"
+						/>
+						Low
+					</label>
+					<br />
+				</div>
+				<button
+					style={
+						!isValid ? { backgroundColor: "red" } : { backgroundColor: "black" }
+					}
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
 export default ReportBug;
